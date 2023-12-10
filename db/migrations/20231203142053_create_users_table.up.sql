0373ad517f25b7ee9143808b72591a8167a237f2
Annisa Syifahus Sholihah <annisaasyifahus@gmail.com>
2023-12-11 05:59:27 +0700

    db rapi

--- db/migrations/20231203142053_create_users_table.up.sql
+++ db/migrations/20231203142053_create_users_table.up.sql
@@ -1,14 +1,14 @@
 BEGIN;
-CREATE TABLE IF NOT EXISTS "public"."users" (
-    "id" SERIAL NOT NULL PRIMARY KEY,
-    "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-    "number" varchar(255) NOT NULL,
-    "email" varchar(255) NOT NULL,
-    "role" varchar(255) NOT NULL,
-    "password" varchar(255) NOT NULL,
-    "wallet" integer NOT NULL DEFAULT 0,
-    "created_at" timestamptz (6),
-    "updated_at" timestamptz (6),
-    "deleted_at" timestamptz (6)
+-- Tabel users
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    number VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    wallet INTEGER NOT NULL DEFAULT 0,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMPTZ
 );
-COMMIT;
--- entity/ticket.go
+++ entity/ticket.go
@@ -3,23 +3,23 @@ package entity
 import "time"
 
 type Ticket struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Price       int64  `json:"price"`
-	Status      bool   `json:"status"`
-	Image       string `json:"image"`
-	Location    string `json:"location"`
-	Quantity    int    `json:"quantity"`
-	Category    string `json:"category"`
-	Date        string
-	Sold        int64     `json:"sold"`
-	CreatedAt   time.Time `json:"-"`
-	UpdatedAt   time.Time `json:"-"`
-	DeletedAt   time.Time `json:"-"`
+	ID          int        `json:"id"`
+	Name        string     `json:"name"`
+	Description string     `json:"description"`
+	Price       int64      `json:"price"`
+	Status      bool       `json:"status"`
+	Image       string     `json:"image"`
+	Location    string     `json:"location"`
+	Quantity    int        `json:"quantity"`
+	Category    string     `json:"category"`
+	Date        time.Time  `json:"date"` // Menggunakan time.Time untuk kolom date
+	Sold        int64      `json:"sold"`
+	CreatedAt   time.Time  `json:"-"`
+	UpdatedAt   time.Time  `json:"-"`
+	DeletedAt   *time.Time `json:"-"`
 }
 
-func Newticket(name string, description string, price int64, status bool, image string, location string, quantity int, category string, date string, sold int64) *Ticket {
+func NewTicket(name string, description string, price int64, status bool, image string, location string, quantity int, category string, date time.Time, sold int64) *Ticket {
 	return &Ticket{
 		Name:        name,
 		Description: description,
@@ -34,7 +34,7 @@ func Newticket(name string, description string, price int64, status bool, image 
 	}
 }
 
-func UpdateTicket(id int, name string, description string, price int64, status bool, image string, location string, quantity int, category string, date string, sold int64) *Ticket {
+func UpdateTicket(id int, name string, description string, price int64, status bool, image string, location string, quantity int, category string, date time.Time, sold int64) *Ticket {
 	return &Ticket{
 		ID:          id,
 		Name:        name,
