c609cf3bcbc89d8b871553e0d00058b47d38549b
Annisa Syifahus Sholihah <annisaasyifahus@gmail.com>
2023-12-10 16:08:43 +0700

    Update repository

--- internal/service/user.service.go
+++ internal/service/user.service.go
@@ -9,6 +9,20 @@ import (
 )
 
 type UserUseCase interface {
+	// FindAll(ctx context.Context) ([]*entity.User, error)
+
+	// Update(ctx context.Context, user *entity.User)
+	// Delete(ctx context.Context, id int64) error
+
+	FindByEmail(ctx context.Context, email string) (*entity.User, error)
+	// UpdateProfile(ctx context.Context, user *entity.User) error
+	// UpdateUserBalance(ctx context.Context, user *entity.User) error
+	// GetProfile(ctx context.Context, userID int64) (*entity.User, error)
+	// GetUserBalance(ctx context.Context, userID int64) (*entity.User, error)
+	// DeleteAccount(ctx context.Context, email string) error
+	// UpgradeWallet(ctx context.Context, user *entity.User) error
+	// UserLogout(ctx context.Context, user *entity.User) error
+	// UpdateWallet(ctx context.Context, userID int64, updatedWallet int64) error
 	GetAll(ctx context.Context) ([]*entity.User, error)
 	CreateUser(ctx context.Context, user *entity.User) error
 	UpdateUser(ctx context.Context, user *entity.User) error
@@ -26,95 +40,41 @@ type UserUseCase interface {
 }
 
 type UserRepository interface {
-	FindAll(ctx context.Context) ([]*entity.User, error)
-	Create(ctx context.Context, user *entity.User) error
-	Update(ctx context.Context, user *entity.User) error
+	GetAll(ctx context.Context) ([]*entity.User, error)
+	CreateUser(ctx context.Context, user *entity.User) error
+	UpdateUser(ctx context.Context, user *entity.User) error
+	GetUserByID(ctx context.Context, id int64) (*entity.User, error)
 	Delete(ctx context.Context, id int64) error
+	UpdateProfile(ctx context.Context, user *entity.User) error
+	UpdateUserBalance(ctx context.Context, user *entity.User) error
+	GetProfile(ctx context.Context, userID int64) (*entity.User, error)
+	GetUserBalance(ctx context.Context, userID int64) (*entity.User, error)
+	DeleteAccount(ctx context.Context, email string) error
+	UpgradeWallet(ctx context.Context, user *entity.User) error
+	UserLogout(ctx context.Context, user *entity.User) error
+	UpdateWallet(ctx context.Context, userID int64, updatedWallet int64) error
 	FindByID(ctx context.Context, id int64) (*entity.User, error)
+	FindByEmail(ctx context.Context, email string) (*entity.User, error)
 }
 
 type UserService struct {
 	repository UserRepository
 }
 
-// CreateUser implements UserUseCase.
-func (*UserService) CreateUser(ctx context.Context, user *entity.User) error {
-	panic("unimplemented")
-}
-
-// DeleteAccount implements UserUseCase.
-func (*UserService) DeleteAccount(ctx context.Context, email string) error {
-	panic("unimplemented")
-}
-
-// GetAll implements UserUseCase.
-func (*UserService) GetAll(ctx context.Context) ([]*entity.User, error) {
-	panic("unimplemented")
-}
-
-// GetProfile implements UserUseCase.
-func (*UserService) GetProfile(ctx context.Context, userID int64) (*entity.User, error) {
-	panic("unimplemented")
-}
-
-// GetUserBalance implements UserUseCase.
-func (*UserService) GetUserBalance(ctx context.Context, userID int64) (*entity.User, error) {
-	panic("unimplemented")
-}
-
-// GetUserByID implements UserUseCase.
-func (*UserService) GetUserByID(ctx context.Context, id int64) (*entity.User, error) {
-	panic("unimplemented")
-}
-
-// UpdateProfile implements UserUseCase.
-func (*UserService) UpdateProfile(ctx context.Context, user *entity.User) error {
-	panic("unimplemented")
-}
-
-// UpdateWallet implements UserUseCase.
-func (*UserService) UpdateWallet(ctx context.Context, userID int64, updatedWallet int64) error {
-	panic("unimplemented")
-}
-
-// UpdateUser implements UserUseCase.
-func (*UserService) UpdateUser(ctx context.Context, user *entity.User) error {
-	panic("unimplemented")
-}
-
-// UpdateUserBalance implements UserUseCase.
-func (*UserService) UpdateUserBalance(ctx context.Context, user *entity.User) error {
-	panic("unimplemented")
-}
-
-// UpgradeWallet implements UserUseCase.
-func (*UserService) UpgradeWallet(ctx context.Context, user *entity.User) error {
-	panic("unimplemented")
-}
-
-// UserLogout implements UserUseCase.
-func (*UserService) UserLogout(ctx context.Context, user *entity.User) error {
-	panic("unimplemented")
-}
-
 func NewUserService(repository UserRepository) *UserService {
 	return &UserService{repository}
 }
 
-func (s *UserService) FindAll(ctx context.Context) ([]*entity.User, error) {
-	return s.repository.FindAll(ctx)
-}
-
-func (s *UserService) Create(ctx context.Context, user *entity.User) error {
+func (s *UserService) CreateUser(ctx context.Context, user *entity.User) error {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
 		return err
 	}
 	user.Password = string(hashedPassword)
-	return s.repository.Create(ctx, user)
+	return s.repository.CreateUser(ctx, user)
 }
 
-func (s *UserService) Update(ctx context.Context, user *entity.User) error {
+func (s *UserService) UpdateUser(ctx context.Context, user *entity.User) error {
 	if user.Role != "" {
 		if user.Role != "Administrator" && user.Role != "Buyer" {
 			return errors.New("role harus di isi Administrator / Buyer")
@@ -127,7 +87,7 @@ func (s *UserService) Update(ctx context.Context, user *entity.User) error {
 		}
 		user.Password = string(hashedPassword)
 	}
-	return s.repository.Update(ctx, user)
+	return s.repository.UpdateUser(ctx, user)
 }
 
 func (s *UserService) Delete(ctx context.Context, id int64) error {
@@ -136,4 +96,72 @@ func (s *UserService) Delete(ctx context.Context, id int64) error {
 
 func (s *UserService) FindByID(ctx context.Context, id int64) (*entity.User, error) {
 	return s.repository.FindByID(ctx, id)
+}
+
+func (s *UserService) DeleteAccount(ctx context.Context, email string) error {
+	return s.DeleteAccount(ctx, email)
+}
+
+// GetAll implements UserUseCase.
+func (s *UserService) GetAll(ctx context.Context) ([]*entity.User, error) {
+	users, err := s.repository.GetAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+// GetProfile implements UserUseCase.
+func (s *UserService) GetProfile(ctx context.Context, userID int64) (*entity.User, error) {
+	return s.repository.GetProfile(ctx, userID)
+}
+
+// GetUserBalance implements UserUseCase.
+func (s *UserService) GetUserBalance(ctx context.Context, userID int64) (*entity.User, error) {
+	return s.repository.GetUserBalance(ctx, userID)
+}
+
+// GetUserByID implements UserUseCase.
+func (s *UserService) GetUserByID(ctx context.Context, id int64) (*entity.User, error) {
+	return s.repository.GetUserByID(ctx, id)
+}
+
+// UpdateProfile implements UserUseCase.
+func (s *UserService) UpdateProfile(ctx context.Context, user *entity.User) error {
+	if user.Name == "" {
+		return errors.New("Nama harus diisi")
+	}
+	if user.Email == "" {
+		return errors.New("Email harus diisi")
+	}
+	return s.repository.UpdateProfile(ctx, user)
+}
+
+// UpdateWallet implements UserUseCase.
+func (s *UserService) UpdateWallet(ctx context.Context, userID int64, updatedWallet int64) error {
+	return s.repository.UpdateWallet(ctx, userID, updatedWallet)
+}
+
+// UpdateUserBalance implements UserUseCase.
+func (s *UserService) UpdateUserBalance(ctx context.Context, user *entity.User) error {
+	if user.Wallet < 0 {
+		return errors.New("Saldo tidak boleh bernilai negatif")
+	}
+	return s.repository.UpdateUserBalance(ctx, user)
+}
+
+// UpgradeWallet implements UserUseCase.
+func (s *UserService) UpgradeWallet(ctx context.Context, user *entity.User) error {
+	return s.repository.UpgradeWallet(ctx, user)
+}
+
+// UserLogout implements UserUseCase.
+func (s *UserService) UserLogout(ctx context.Context, user *entity.User) error {
+	return s.repository.UserLogout(ctx, user)
+
+}
+
+func (s *UserService) FindByEmail(ctx context.Context, email string) (*entity.User, error) {
+	return s.repository.FindByEmail(ctx, email)
 }
